316d8be236b3a4c23f588a1d1f7a833bab9e42cb
XMediator CI <vision-team@x3mads.com>
2026-09-23 20:49:28 +0000

    Release 5.8.0

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "LevelPlayBidMachineAdapter", targets: ["LevelPlayBidMachineAdapterWrapper"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ironsource-mobile/LevelPlay-BidMachine-Adapter-Swift-Package.git", exact: "5.7.1"),
+        .package(url: "https://github.com/ironsource-mobile/LevelPlay-BidMachine-Adapter-Swift-Package.git", exact: "5.8.0"),
     ],
     targets: [
         .target(
--- README.md
+++ README.md
@@ -18,7 +18,7 @@ coexist.
 Add this repository as a package dependency and select the
 `LevelPlayBidMachineAdapter` product instead of LevelPlay's `BidMachineAdapter`.
 Versions match the LevelPlay adapter versions one-to-one: version
-`5.7.1` of this package uses LevelPlay adapter `5.7.1`.
+`5.8.0` of this package uses LevelPlay adapter `5.8.0`.
 
 ## License
 
